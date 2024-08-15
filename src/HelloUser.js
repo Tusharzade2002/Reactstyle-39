c99b049b724f6b1f41e46617ffe5d4b93baa21a8
Tusharzade2002 <tusharzade29@gmail.com>
2024-08-15 09:38:19 +0530

    props use

--- src/HelloUser.js
+++ src/HelloUser.js
@@ -1,9 +1,19 @@
-import React from 'react'
 
-function HelloUser() {
-  return (
-    <div>HelloUser</div>
-  )
+
+function HelloUser(props){
+     return(
+        <div>
+            Hello {props.name}
+        </div>
+     )
 }
-
 export default HelloUser
+
+
+function GoodMoring(props){
+    return(
+        <div>
+            GoodMoring {props.name}
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import HelloUser  from './HelloUser';
+import GoodMorning from './HelloUser'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
-  <HelloUser />
+  <div>
+  <HelloUser name="Tushar"  />
+  <HelloUser name="saurabh" />
+  <GoodMorning name="Krishna"/>
+  </div>
 );
 
 
