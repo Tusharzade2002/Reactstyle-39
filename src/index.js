0df091772aa9caad9e91934e5024f9cb66343722
Tusharzade2002 <tusharzade29@gmail.com>
2024-08-15 10:11:20 +0530

    apply css in heading

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import HelloUser, { GoodEvening, GoodMoring } from './HelloUser';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
   <div>
-     <h1 >ALL GRETTING</h1>
+     <h1 style={{
+         textAlign:"center",
+         color:"tomato",
+     }}>ALL GRETTING</h1>
   <HelloUser name="Tushar"  />
   <GoodMoring name="Saurabh" />
   <GoodEvening  name="Krishna"/>
